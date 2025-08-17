84b6a32b48b8ad7bb9df1da2e1ae721e9ddb6355
abhay-byte <abhay02delhi@gmail.com>
2025-08-17 22:35:38 +0530

    update: tweaks

--- scripts/termux-tweaks-standalone.sh
+++ scripts/termux-tweaks-standalone.sh
@@ -2,12 +2,16 @@
 
 BASE_URL="https://raw.githubusercontent.com/abhay-byte/Linux_Setup/dev"
 
-curl -fsSL "$BASE_URL/scripts/install-dependecies.sh" | bash
+curl -fsSL "$BASE_URL/scripts/install-dependecies.sh" | sh
 
-read -rp "Do you want to install Termux tweaks? (y/n): " tweaks_choice
-if [[ "$tweaks_choice" =~ ^[Yy]$ ]]; then
-    echo "Installing Termux tweaks..."
-    curl -fsSL "$BASE_URL/scripts/termux-tweaks.sh" | bash
-else
-    echo "Skipping Termux tweaks."
-fi
+echo -n "Do you want to install Termux tweaks? (y/n): "
+read tweaks_choice
+case "$tweaks_choice" in
+    [Yy]*)
+        echo "Installing Termux tweaks..."
+        bash <(curl -fsSL "$BASE_URL/scripts/termux-tweaks.sh")
+        ;;
+    *)
+        echo "Skipping Termux tweaks."
+        ;;
+esac
